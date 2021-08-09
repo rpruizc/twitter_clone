5e2be5fae489481ad5fe551fb2555854ec3e685c
Rodolfo Ruiz <rpruiz@me.com>
2021-08-09 07:24:17 -0500

    likes and tweets tables with diesel

--- migrations/00000000000000_diesel_initial_setup/down.sql
+++ migrations/00000000000000_diesel_initial_setup/down.sql
@@ -4,3 +4,6 @@
 
 DROP FUNCTION IF EXISTS diesel_manage_updated_at(_tbl regclass);
 DROP FUNCTION IF EXISTS diesel_set_updated_at();
+
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS likes;
--- migrations/00000000000000_diesel_initial_setup/up.sql
+++ migrations/00000000000000_diesel_initial_setup/up.sql
@@ -1,21 +1,3 @@
--- This file was automatically created by Diesel to setup helper functions
--- and other internal bookkeeping. This file is safe to edit, any future
--- changes will be added to existing projects as new migrations.
-
-
-
-
--- Sets up a trigger for the given table to automatically set a column called
--- `updated_at` whenever the row is modified (unless `updated_at` was included
--- in the modified columns)
---
--- # Example
---
--- ```sql
--- CREATE TABLE users (id SERIAL PRIMARY KEY, updated_at TIMESTAMP NOT NULL DEFAULT NOW());
---
--- SELECT diesel_manage_updated_at('users');
--- ```
 CREATE OR REPLACE FUNCTION diesel_manage_updated_at(_tbl regclass) RETURNS VOID AS $$
 BEGIN
     EXECUTE format('CREATE TRIGGER set_updated_at BEFORE UPDATE ON %s
@@ -34,3 +16,15 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TABLE IF NOT EXISTS tweets (
+    id          UUID PRIMARY KEY        NOT NULL,
+    created_at  TIMESTAMP DEFAULT now() NOT NULL,
+    message     text                    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMP DEFAULT now() NOT NULL,
+    tweet_id    UUID                    NOT NULL REFERENCES tweets(id)
+);
