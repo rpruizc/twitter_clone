738d71eb4df35d6fc165fbaf36116a5a657138f8
Rodolfo Ruiz <rpruiz@me.com>
2021-08-09 08:16:24 -0500

    DB setup and connection main code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #[macro_use]
 extern crate actix_web;
 
+#[macro_use]
+extern crate diesel;
+
 use actix_web::{App, HttpServer, middleware};
+use diesel::r2d2::ConnectionManager;
+use diesel::PgConnection;
+use r2d2::{Pool, PooledConnection};
 use std::{env, io};
 
 mod constants;
 mod like;
 mod response;
+mod schema;
 mod tweet;
 
 #[actix_rt::main]
@@ -14,8 +21,16 @@ async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug, actix_server=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    // Database setup
+    let database_url = env::var("DATABASE_URL").expect("DB URL NOT SET");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(middleware::Logger::default())
             .service(tweet::list)
             .service(tweet::get)
